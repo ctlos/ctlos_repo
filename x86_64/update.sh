515d3229e77b7514b878093714c38e539d18e6a8
creio <mailcreio@gmail.com>
2020-07-14 10:56:56 +0300

    2020.07.14 Update

--- x86_64/update.sh
+++ x86_64/update.sh
@@ -7,7 +7,7 @@ repo-add -s -v -n -R ctlos_repo.db.tar.gz *.pkg.tar.{xz,zst}
 # repo-add -s -n -R ctlos_repo.db.tar.gz *.pkg.tar.zst
 
 # rm ctlos_repo.db
-cp -f ctlos_repo.db.tar.gz ctlos_repo.db
+# cp -f ctlos_repo.db.tar.gz ctlos_repo.db
 ##optional-remove for old repo.db##
 rm *gz.old{,.sig}
 
