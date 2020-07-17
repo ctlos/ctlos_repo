e5b07d4038fd702f5c079f28ca1c201a1fa4c2f4
creio <mailcreio@gmail.com>
2020-07-17 17:37:09 +0300

    17.07.2020 Update

--- x86_64/update.sh
+++ x86_64/update.sh
@@ -10,6 +10,6 @@ cp -f ctlos_repo.db.tar.gz ctlos_repo.db
 ##optional-remove for old repo.db##
 # rm *gz.old{,.sig}
 
-# rsync -auvC -L -P --delete-excluded --delete /media/files/github/ctlos/ctlos_repo/x86_64 creio@storage.osdn.net:/storage/groups/c/ct/ctlos/ctlos_repo/
+rsync -auvC -L -P --delete-excluded --delete /media/files/github/ctlos/ctlos_repo/x86_64 creio@storage.osdn.net:/storage/groups/c/ct/ctlos/ctlos_repo/
 
 echo "Repo Up"
