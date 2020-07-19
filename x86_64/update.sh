91ac1bae27ecd4cde7d8f5114b684fd619501592
creio <mailcreio@gmail.com>
2020-07-19 22:16:10 +0300

    19.07.2020 Update

--- x86_64/update.sh
+++ x86_64/update.sh
@@ -11,19 +11,23 @@ if [ "$1" = "-add" ]; then
   cp -f ctlos_repo.db.tar.gz ctlos_repo.db
   ##optional-remove for old repo.db##
   # rm *gz.old{,.sig}
+echo "Repo Up"
 elif [ "$1" = "-clean" ]; then
   rm ctlos_repo*
+echo "Repo clean"
 elif [ "$1" = "-o" ]; then
   rsync -auvCLP --delete-excluded --delete "$local_repo" "$dest_osdn"
+echo "rsync osdn repo"
 elif [ "$1" = "-k" ]; then
   systemctl start --user kbfs
   rsync -auvCLP --delete-excluded --delete "$local_repo" "$dest_keybase"
+echo "rsync keybase repo"
 elif [ "$1" = "-all" ]; then
   rsync -auvCLP --delete-excluded --delete "$local_repo" "$dest_osdn"
   systemctl start --user kbfs
   rsync -auvCLP --delete-excluded --delete "$local_repo" "$dest_keybase"
+echo "rsync all repo"
 else
   echo "No rsync repo"
 fi
 
-echo "Repo Up"
