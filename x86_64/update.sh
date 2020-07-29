abb313e94e870cc4bcc7011a3ab98a273549bdd9
creio <mailcreio@gmail.com>
2020-07-29 20:47:48 +0300

    29.07.2020 Update

--- x86_64/update.sh
+++ x86_64/update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-local_repo=/media/files/github/ctlos/ctlos_repo/x86_64
+local_repo=/media/files/github/ctlos/ctlos_repo/
 dest_osdn=creio@storage.osdn.net:/storage/groups/c/ct/ctlos/ctlos_repo/
 dest_keybase=/run/user/1001/keybase/kbfs/public/cvc/ctlos_repo/
 
@@ -16,16 +16,16 @@ elif [ "$1" = "-clean" ]; then
   rm ctlos_repo*
 echo "Repo clean"
 elif [ "$1" = "-o" ]; then
-  rsync -auvCLP --delete-excluded --delete "$local_repo" "$dest_osdn"
+  rsync -auvCLP --delete-excluded --delete "$local_repo"x86_64 "$dest_osdn"
 echo "rsync osdn repo"
 elif [ "$1" = "-k" ]; then
   systemctl start --user kbfs
-  rsync -auvCLP --delete-excluded --delete "$local_repo" "$dest_keybase"
+  rsync -auvCLP --delete-excluded --delete --exclude={"build",".git*",".*ignore"} "$local_repo" "$dest_keybase"
 echo "rsync keybase repo"
 elif [ "$1" = "-all" ]; then
-  rsync -auvCLP --delete-excluded --delete "$local_repo" "$dest_osdn"
+  rsync -auvCLP --delete-excluded --delete "$local_repo"x86_64 "$dest_osdn"
   systemctl start --user kbfs && \
-  rsync -auvCLP --delete-excluded --delete "$local_repo" "$dest_keybase"
+  rsync -auvCLP --delete-excluded --delete --exclude={"build",".git*",".*ignore"} "$local_repo" "$dest_keybase"
 echo "rsync all repo"
 else
   echo "No rsync repo"
